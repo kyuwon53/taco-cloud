2560cb930a4886c644586e49f8d1d5a8bd65b290
leekyuwon <kyuwoon369@gmail.com>
2023-05-31 23:07:03 +0900

    spring in action day 5

--- src/main/java/com/springinaction/tacocloud/Order.java
+++ src/main/java/com/springinaction/tacocloud/Order.java
@@ -7,8 +7,12 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.util.Date;
+
 @Data
 public class Order {
+    private Long id;
+    private Date placeAt;
     @NotBlank(message = "Name is required")
     private String deliveryName;
 
